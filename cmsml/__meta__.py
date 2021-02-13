0c3649295f598302927a111ffcbbdabe228747c2
Marcel R <github.riga@icloud.com>
2021-02-13 14:00:36 +0100

    Version 0.1.0.

--- cmsml/__meta__.py
+++ cmsml/__meta__.py
@@ -7,10 +7,10 @@ Python package of the CMS Machine Learning Group.
 
 __author__ = "CMS Machine Learning Group"
 __email__ = "hn-cms-machine-learning@cern.ch"
-__copyright__ = "Copyright 2020, CMS Machine Learning Group"
+__copyright__ = "Copyright 2020-2021, CMS Machine Learning Group"
 __credits__ = ["Marcel Rieger"]
 __contact__ = "https://github.com/cms-ml/cmsml"
 __license__ = "BSD-3-Clause"
 __status__ = "Development"
-__version__ = "0.0.1"
+__version__ = "0.1.0"
 __all__ = []
