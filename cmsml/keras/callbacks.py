4b2c007f36c9422182795a3a2f4cdd2710f7137a
Marcel R <github.riga@icloud.com>
2021-02-25 13:28:24 +0100

    Add first keras callback.

---
+++ cmsml/keras/callbacks.py
@@ -0,0 +1,40 @@
+# coding: utf-8
+
+"""
+Custom keras callbacks.
+"""
+
+__all__ = ["GPUStatsLogger"]
+
+
+from cmsml.util import make_list, verbose_import
+
+import tensorflow as tf
+
+
+class GPUStatsLogger(tf.keras.callbacks.Callback):
+
+    def __init__(self, *args, **kwargs):
+        device_numbers = make_list(kwargs.pop("device_numbers", [0]))
+
+        super(GPUStatsLogger, self).__init__(*args, **kwargs)
+
+        # setup py3nvml
+        self.smi = verbose_import("py3nvml", pip_name="py3nvml").py3nvml
+        self.smi.nvmlInit()
+
+        # get device handles
+        self.device_numbers = device_numbers
+        self.handles = [self.smi.nvmlDeviceGetHandleByIndex(n) for n in self.device_numbers]
+
+    def _log_usage(self, logs):
+        for n, handle in zip(self.device_numbers, self.handles):
+            mem = self.smi.nvmlDeviceGetMemoryInfo(handle)
+            res = self.smi.nvmlDeviceGetUtilizationRates(handle)
+            logs["GPU {} usage [%]".format(n)] = res.gpu
+            logs["GPU {} vRAM [%]".format(n)] = 100 * mem.used / mem.total
+            logs["GPU {} vRAM [MB]".format(n)] = mem.used / (1024**2)
+
+    def on_batch_end(self, x, logs=None):
+        if logs is not None:
+            self._log_usage(logs)
--- cmsml/util.py
+++ cmsml/util.py
@@ -4,13 +4,55 @@
 Helpful functions and utilities.
 """
 
-__all__ = ["tmp_file", "tmp_dir"]
+__all__ = [
+    "is_lazy_iterable", "make_list", "tmp_file", "tmp_dir",
+]
 
 
 import os
 import shutil
 import tempfile
 import contextlib
+import types
+import collections
+import importlib
+
+import six
+
+
+def is_lazy_iterable(obj):
+    """
+    Returns whether *obj* is iterable lazily, such as generators, range objects, maps, etc.
+    """
+    iter_types = (
+        types.GeneratorType, collections.MappingView, six.moves.range, six.moves.map, enumerate,
+    )
+    return isinstance(obj, iter_types)
+
+
+def make_list(obj, cast=True):
+    """
+    Converts an object *obj* to a list and returns it. Objects of types *tuple* and *set* are
+    converted if *cast* is *True*. Otherwise, and for all other types, *obj* is put in a new list.
+    """
+    if isinstance(obj, list):
+        return list(obj)
+    elif is_lazy_iterable(obj):
+        return list(obj)
+    elif isinstance(obj, (tuple, set)) and cast:
+        return list(obj)
+    else:
+        return [obj]
+
+
+def verbose_import(user, module_name, package=None, pip_name=None):
+    try:
+        return importlib.import_module(module_name, package=package)
+    except ImportError as e:
+        e.msg += " but is required by {}".format(user)
+        if pip_name:
+            e.msg += " (you may want to try 'pip install --user {}')".format(pip_name)
+        raise
 
 
 @contextlib.contextmanager
