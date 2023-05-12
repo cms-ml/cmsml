9485cb7650a8c91be4af9fac570672e4c46f24e9
Bogdan W <bogdan.wiederspan@desy.de>
2023-05-12 14:50:09 +0200

    Removed many function. This was possible by found a way using signatures to convert tf and keras the same way.

--- cmsml/to_integrate/convert_model.py
+++ cmsml/to_integrate/convert_model.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 from pathlib import Path
 import tensorflow as tf
 import os
+from typing import Sequence, Union
 
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
 
@@ -13,11 +14,10 @@ class ModelConverter():
                  dst_dir: str = "",
                  serving_key: str = "serving_default",
                  batch_sizes: str = "1",
-                 inputs = "input:0"
                  ):
         """
         Create an ModelConverter instance which holds all meta information necessary to
-        make a graph static. To start the conversion process call the instance.
+        make a graph static. To start the conversion process call self.save_static_graph.
         The static graph is saved within the .pb at *dst* using the
         serving_key: "batch_size_N", with N beeing your *batch_sizes*.
 
@@ -30,15 +30,15 @@ class ModelConverter():
             batch_sizes (str, optional): String with batch sizes (delimited by space)
             the network should convert to
         """
-        self.src = self._is_saved_model(src_dir)
-        self.dst = self._parse_dst(self.src, dst_dir)
-        self.serving_key = serving_key
-        self.batch_sizes = self._map_batch_sizes(batch_sizes)  # create sequence of ints
+        self.src, self.dst = self.check_paths(src_dir, dst_dir)
         self.is_keras_model = self._is_keras(self.src)  # set keras flag
+
         self.model = self.load_model()  # the actual saved model
-        self.graph = self._extract_graph()  # the concrete function (graph) of the model
+        self.serving_key = serving_key
+        self.graph = self.model.signatures[serving_key]  # the concrete function (graph) of the model
+
+        self.batch_sizes = self._map_batch_sizes(batch_sizes)  # create sequence of ints
         self.signatures = None  # the static signatures, after the extration
-        self.input_names = (inputs,) if isinstance(inputs,str) else inputs
 
     @staticmethod
     def _pretty_color(string: str, color: str = "white") -> str:
@@ -77,66 +77,73 @@ class ModelConverter():
     def _print_warning(string: str):
         print(ModelConverter._pretty_color(string, "red"))
 
-    @staticmethod
-    def _is_saved_model(path: str) -> Path:
-        """Checks if saved model exists under path, returns a Path Object.
-        To actually get the str use str(Path).
 
-        Args:
-            path (str): Path to saved model
 
-        Returns:
-            Path: Posix or WindowsPath of path
+    def check_paths(self, src: str, dst: str) -> tuple(Path, Path):
 
-        Raises:
-            ValueError: If path does not exists
-        """
-        path = Path(path)
-        if not path.exists():
-            raise ValueError(f"Directory does not exists: \n {str(path)}")
-        if not tf.saved_model.contains_saved_model(str(path)):
-            raise ValueError(f"Directory: {str(path)} does not contain a tensorflow saved_model")
-        return path
+        def is_saved_model(src: str) -> Path:
+            """Helper function to if valid saved model exists under *src*
+            Args:
+                src (str): Path to saved model
 
-    @staticmethod
-    def _parse_dst(src: str, dst: str) -> Path:
-        """Summary
+            Returns:
+                Path: Posix or WindowsPath of path
 
-        Args:
-            src (str): Description
-            dst (str): Description
+            Raises:
+                ValueError: If path does not exists
+            """
+            path = Path(src)
+            if not path.exists():
+                raise ValueError(f"Directory does not exists: \n {str(path)}")
+            if not tf.saved_model.contains_saved_model(str(path)):
+                raise ValueError(f"Directory: {str(path)} does not contain a tensorflow saved_model")
+            return path
 
-        Returns:
-            Path: Description
-        """
-        if dst == "":
-            # when no destination is given use alternate source
-            dst = src.with_name("".join((src.name, "_static")))
-        else:
-            if dst == "same":
-                dst = src
-        return Path(dst)
+
+        def parse_dst(src: Union[str, Path], dst: Union[str, Path]) -> Path:
+            """Helper function to append dst with default name and preventing overwriting the source model
+
+            Args:
+                src (str): Source of model
+                dst (str): Destination of static model
+
+            Returns:
+                Path: Path Object of destination of static model
+            """
+            if dst == "":
+                # when no destination is given use alternate source path
+                dst = src.with_name("".join((src.name, "_static")))
+            else:
+                # prevent overwriting src
+                if Path(dst) == Path(src):
+                    msg = "You can not overwrite your source model. Please choose another *dst*"
+                    raise ValueError(msg)
+            return Path(dst)
+        return is_saved_model(src), parse_dst(src, dst)
 
     @staticmethod
     def _map_batch_sizes(batch_sizes: str | [list, tuple]):
         # maps str with space delimiter to tuple with int"s
         # floats are converted to int
         if isinstance(batch_sizes, str):
-            # will fail if you use wrong delimiter
-            bs = tuple(map(int, batch_sizes.split(" ")))
+            # fail if you use wrong delimiter
+            try:
+                batch_size = tuple(map(int, batch_sizes.split(" ")))
+            except:
+                raise("Only single space are accepted delimiter for batch sizes")
         elif isinstance(batch_sizes, (tuple, list)):
-            bs = tuple(map(int, batch_sizes))
+            batch_size = tuple(map(int, batch_sizes))
         else:
             msg = f"""Batch sizes type is {type(batch_sizes)}, but needs to be
-            of type [tuple, list] or str which is delimited by space.
+            of type [tuple, list] or str delimited by space.
             """
-            raise ValueError(msg)
+            raise TypeError(msg)
 
         # clean up double values, but preserve order
-        cleaned_bs = tuple(sorted(set(bs), key=bs.index))
-        if len(cleaned_bs) != len(bs):
+        cleaned_batch_size = tuple(sorted(set(batch_size), key=batch_size.index))
+        if len(cleaned_batch_size) != len(batch_size):
             ModelConverter._print_notation("Doubled batch sizes are detected and removed.")
-        return cleaned_bs
+        return cleaned_batch_size
 
     @staticmethod
     def _is_keras(model_path: Path) -> bool:
@@ -155,33 +162,6 @@ class ModelConverter():
         ModelConverter._print_success(f"Model within Path is a Keras Model: {keras_model_exists}")
         return keras_model_exists
 
-    @property
-    def inputs(self) -> list[str]:
-        """Get the inputs of the loaded model/graph as Sequence of ints
-
-        Returns:
-            Sequence[str]: List with input names
-        """
-        # if self.input_names:
-        #     return {for }
-
-        # we are only interested in the tensorspecs of the inputs
-        if self.is_keras_model:
-            # keras inputs function returns correct order of inputs!
-            return [input.type_spec for input in self.model.inputs]
-        else:
-            # all other serialized node information are not ordered!
-            # _arg_keywords, sadly returns unordered input names
-            # TODO: search right way.
-            # overwrite order by own names
-            correct_ordered_inputs = self.input_names if self.input_names else self.graph._arg_keywords
-
-
-            # tuple with dict with unsorted input nodes: NODE_NAME:TensorSpec
-            # first entry is empty
-            graph_inputs = self.graph.structured_input_signature[1]
-            return [graph_inputs[input_node_name] for input_node_name in correct_ordered_inputs]
-
 
     def load_model(self):
         """Loads the model using self.src.
@@ -191,31 +171,18 @@ class ModelConverter():
         """
         model_path = str(self.src)
         self._print_notation(f"Load model: {model_path}")
-        if self.is_keras_model:
-            name = "Keras"
-            model = tf.keras.models.load_model(model_path)
-        else:
-            name = "Tensorflow"
-            model = tf.saved_model.load(model_path)
-        self._print_success(f"Succesfully loaded {name} model")
+        with tf.device('/cpu:0'):
+            if self.is_keras_model:
+                name = "Keras"
+                model = tf.keras.models.load_model(model_path)
+            else:
+                name = "Tensorflow"
+                model = tf.saved_model.load(model_path)
+            self._print_success(f"Succesfully loaded {name} model")
         return model
 
-    def _extract_graph(self) -> tf.SignatureDef:
-        """Extract graph if the model is NOT a keras model.
 
-        Returns:
-            tf.SignatureDef: Inference function if model is a keras model otherwise None
-        """
-        if not self.is_keras_model:
-            inference_func = self.model.signatures[self.serving_key]
-            # there is a bug for TFV < 2.8 where python garbage collector kills the model
-            # due to having no weak link. This is not a nice solution but it works.
-            inference_func._backref_to_saved_model = self.model
-            return inference_func
-        else:
-            return None
-
-    def _static_tensorspecs(self, batch_size: int) -> tf.TensorSpec:
+    def _static_tensorspecs(self, batch_size: int) -> dict[tf.TensorSpec]:
         """
         Takes the input layers of loaded model or graph and creates a
         Tensorspecs with static <batch_size> Shape and new name (batch_size_<batch_size>).
@@ -226,8 +193,10 @@ class ModelConverter():
         Returns:
             tf.TensorSpec: The now static shape
         """
-        tensorspecs = []
-        for input_layer in self.inputs:
+        tensorspecs = {}
+        # get inputs and loop throught
+        input_layers = self.graph.structured_input_signature[1]
+        for name, input_layer in input_layers.items():
             shape = input_layer.shape.as_list()
 
             # ignore empty argument shapes
@@ -241,99 +210,53 @@ class ModelConverter():
             # networks : are problematic in name
             new_name = new_name.replace(":", "_")
 
-            layer_description = tf.TensorSpec(shape=tf.TensorShape(shape),
+            tensorspecs[name] = tf.TensorSpec(shape=tf.TensorShape(shape),
                                               dtype=input_layer.dtype,
                                               name=input_layer.name)
-            tensorspecs.append(layer_description)
         return tensorspecs
 
-    def _static_concrete_function(self, batch_size: int) -> tf.types.experimental.ConcreteFunction:
+
+    def create_static_signatures(self: Sequence[int]) -> dict[tf.types.experimental.ConcreteFunction]:
         """
-        Exctract the concrete function assoziated with the static <batch_size>.
-
-        Args:
-            batch_size (int): Description
-
-        Returns:
-            tf.types.experimental.ConcreteFunction: Description
-        """
-        # Creates a static tensorspec and use it to extract the corresponding concrete function
-        static_tensorspec = self._static_tensorspecs(batch_size=batch_size)
-
-        # get the concrete function for the signature: static_tensorspec
-        new_signature = tf.function(
-            self.model.__call__).get_concrete_function(inputs=static_tensorspec, training=False, mask=None)
-        return new_signature
-
-    def create_static_signatures(self):
-        """
+        Exctract the concrete function assoziated with a static <batch_size>.
         Takes a Sequence of ints and returns a dictionary with concrete functions.
         The keys of the dictionary follow \"batch_size_<batch_size>\" template.
         The concrete functions have static batch_size.
         """
         signature_dictionary = {}
-
         # create the concrete functions and fill signature dictionary
-        for bs in self.batch_sizes:
-            save_serving_key = "".join(("batch_size_", str(bs)))
+        for batch_size in self.batch_sizes:
+            save_serving_key = "".join(("batch_size_", str(batch_size)))
             self._print_notation(f"Create signature with servingkey: {save_serving_key}")
-
-            signature = self._static_concrete_function(bs)
-            signature_dictionary[save_serving_key] = signature
-
+            static_tensorspec = self._static_tensorspecs(batch_size=batch_size)
+            signature_dictionary[save_serving_key] = tf.function(self.graph).get_concrete_function(**static_tensorspec)
         # save signature dictionary
         self.signatures = signature_dictionary
+        return signature_dictionary
 
-    def decap_create_static_signatures(self):
-        # works for 1 node input only
-        """
-        Takes a Sequence of ints and returns a dictionary with concrete functions.
-        The keys of the dictionary follow \"batch_size_<batch_size>\" template.
-        The concrete functions have static batch_size.
-        """
-        signature_dictionary = {}
-
-        # create the concrete functions and fill signature dictionary
-        for bs in self.batch_sizes:
-            save_serving_key = "".join(("batch_size_", str(bs)))
-            self._print_notation(f"Create signature with servingkey: {save_serving_key}")
-
-            signature = self._static_concrete_function(bs)
-            signature_dictionary[save_serving_key] = signature
-
-        # save signature dictionary
-        self.signatures = signature_dictionary
-
-    def clear_devices(self, graph_def):
+    def set_device(self, model, serving_key, device="/device:CPU:*"):
         """
         Create new graph with settings of graph_def, but cleared device.
         """
-        # removes all device placement information
-        for node in graph_def.node:
-            node.device = ""
 
-        # create create modified graph
-        graph = tf.Graph()
-        with graph:
-            # load cleared graph_def into new graph
-            tf.graph_util.import_graph_def(graph_def,
-                                           name="")
+        graph = model.signatures[serving_key].graph
+
+        # removes all device placement information
+        for op in graph.get_operations():
+            op._set_device = device
         return graph
 
     def save_signatures(self):
         """
         Save signature as saved_model under <saving_dir>
         """
-
-        self._print_notation(f"Starting save of signatures at {str(self.dst)}:")
-
+        self._print_notation(f"Start saving signatures at {str(self.dst)}:")
         tf.saved_model.save(self.model, str(self.dst),
                             signatures=self.signatures)
-
         print_saving_dir = self._pretty_color(str(self.dst), "red")
         self._print_success(f"Saved the model with static signatures at: {print_saving_dir}")
 
-    def __call__(self):
+    def save_static_graph(self):
         self.create_static_signatures()
         self.save_signatures()
 
@@ -345,7 +268,7 @@ def main(src: str,
     converter = ModelConverter(src, dst,
                                serving_key=serving_key,
                                batch_sizes=batch_sizes)
-    converter()
+    converter.save_static_graph()
 
 
 if __name__ == "__main__":
