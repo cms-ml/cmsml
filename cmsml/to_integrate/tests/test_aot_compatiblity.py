45b90b876acc6cd90b5e58adbbdc5cc99b68954c
Bogdan W <bogdan.wiederspan@desy.de>
2023-06-01 15:49:17 +0200

    added test for aot_compatility

--- cmsml/to_integrate/tests/test_aot_compatiblity.py
+++ cmsml/to_integrate/tests/test_aot_compatiblity.py
@@ -1,36 +1,38 @@
 from __future__ import annotations
 import sys
-sys.path.append("..")
-sys.path.append("../..")
+sys.path.append('..')
+sys.path.append('../..')
 import tensorflow as tf
 
 
 import unittest
 from pathlib import Path
 from util import tmp_dir, tmp_file
-from check_aot_compatibility import OpsTable, ModelLoader, ModelOpsChecker
+import cmsml.tensorflow.tools as cmsml_tools
+from check_aot_compatibility import OpsTable, ModelOpsChecker
 
 
 class TestOpsTable(unittest.TestCase):
-    CPU_2_6_4 = "compat_ops_tables/2_6_4_XLA_CPU_JIT.txt"
-    CPU_TABLE = "compat_ops_tables/XLA_CPU_JIT_old.txt"
-    GPU_TABLE = "compat_ops_tables/XLA_GPU_JIT_old.txt"
+    CPU_TABLE = 'tests/compat_ops_tables/XLA_CPU_JIT_old.txt'
+    GPU_TABLE = 'tests/compat_ops_tables/XLA_GPU_JIT_old.txt'
 
     def setUp(self):
-        self.table = OpsTable()
+        self.table = OpsTable(self.CPU_TABLE, self.GPU_TABLE)
 
+    @unittest.skip
     def test_create_markdown_table(self):
         with tmp_file() as path:
-            file_dst = Path(path) / "test_table"
-            created_file_path = self.table.create_markdown_table(dst=file_dst,
-                                        devices="CPU")
+            file_dst = Path(path) / 'test_table'
+            created_file_path = self.table.create_markdown_table(
+                device='CPU', save_location=file_dst, suffix='.md')
 
+            # save markdown file works
             self.assertTrue(Path(created_file_path).exists)
 
             # TODO GPU currently not supported by CMMSW, remove Raises when implemented
             with self.assertRaises(NotImplementedError):
                 file_path = self.create_markdown_table(dst=file_dst,
-                                        devices="GPU")
+                                        devices='GPU')
                 self.assertTrue(Path(file_path).exists)
 
     def test_read_markdown_table(self):
@@ -39,10 +41,8 @@ class TestOpsTable(unittest.TestCase):
         test_entry_cpu = {
             'Abs': {
                 'op': 'Abs',
-                'cpu': True,
-                'gpu': False,
-                'allowed_types_cpu': 'T={double,float,int32,int64}',
-                'allowed_types_gpu': None,
+                'cpu': 'T={double,float,int32,int64}',
+                'gpu': None,
             },
         }
 
@@ -53,139 +53,129 @@ class TestOpsTable(unittest.TestCase):
         test_entry_gpu = {
             'Abs': {
                 'op': 'Abs',
-                'cpu': False,
-                'gpu': True,
-                'allowed_types_cpu': None,
-                'allowed_types_gpu': 'T={double,float,int32,int64}',
+                'gpu': 'T={double,float,int32,int64}',
+                'cpu': None,
             },
         }
 
         self.assertEqual(test_entry_gpu['Abs'], ops_dict_gpu['Abs'])
 
-    @unittest.skip("TODO Function not fully implemented")
     def test_merge_markdown_tables(self):
         # combine 2 tables information.
         # used to get overview of all possible devices.
-        ops_dict = self.table.merge_markdown_tables(self.CPU_TABLE, self.GPU_TABLE)
         test_entry_merged = {
             'Abs': {
                 'op': 'Abs',
-                'cpu': True,
-                'gpu': True,
-                'allowed_types_cpu': 'T={double,float,int32,int64}',
-                'allowed_types_gpu': 'T={double,float,int32,int64}',
+                'cpu': 'T={double,float,int32,int64}',
+                'gpu': 'T={double,float,int32,int64}',
             },
+            '_XLASend': {'op': '_XLASend',
+            'cpu': 'T={bool,complex64,double,float,int32,int64,uint32,uint64}',
+            'gpu': None},
         }
-        print('\n' * 3)
 
-        for k in test_entry_merged['Abs'].keys():
-            print(test_entry_merged['Abs'][k], ops_dict['Abs'][k])
-
-        print('\n' * 3)
-        self.assertEqual(test_entry_merged['Abs'], ops_dict['Abs'])
-
-    @unittest.skip("TODO Not sure what to test to be honest")
-    def test_get_unique_ops(self):
-        cpu_ops_dict = self.table.read_markdown_table(self.CPU_TABLE)
-        gpu_ops_dict = self.table.read_markdown_table(self.GPU_TABLE)
-
-        all_ops = self.table.get_get_unique_ops()
-
-        with self.assertRaises(Exception):
-            # raise if non-supported op is given
-            not_supported_ops = self.table.get_get_unique_ops(all_ops, "dummy_device")
+        self.assertEqual(test_entry_merged['Abs'], self.table.ops_dict['Abs'])
+        self.assertEqual(test_entry_merged['_XLASend'], self.table.ops_dict['_XLASend'])
 
 
-class TestModelLoader(unittest.TestCase):
-    def setUp(self):
+class TestModelOpsChecker(unittest.TestCase):
+    CPU_TABLE = TestOpsTable.CPU_TABLE
+    GPU_TABLE = TestOpsTable.GPU_TABLE
 
-        test_dir = Path('test_models')
-        self.saved_keras_model = str(test_dir / 'test_keras_model')
-        self.saved_tf_model = str(test_dir / 'test_saved_model')
-        self.saved_graph = str(test_dir / 'test_freeze_graph.pb')
+    @classmethod
+    def setUpClass(cls):
+        # global paths to the temp dirs containing the test models
+        cls.paths = TestModelOpsChecker.save_test_model()
+        cls.serving_key = tf.saved_model.DEFAULT_SERVING_SIGNATURE_DEF_KEY
 
-        self.mm_graph = ModelLoader(self.saved_graph)
-        self.mm_keras_model = ModelLoader(self.saved_keras_model)
-        self.mm_tf_model = ModelLoader(self.saved_tf_model)
+        cls.graph_model_ops = ModelOpsChecker(
+            model_path=TestModelOpsChecker.paths['frozen_graph'], serving_key=cls.serving_key, node_def_number=0)
+        cls.keras_model_ops = ModelOpsChecker(
+            model_path=TestModelOpsChecker.paths['keras'], serving_key=cls.serving_key, node_def_number=0)
+        cls.tf_model_ops = ModelOpsChecker(
+            model_path=TestModelOpsChecker.paths['tf'], serving_key=cls.serving_key, node_def_number=0)
 
-    def create_dummy_model(self):
-        x1 = tf.keras.Input(shape=(2,), name="foo")
-        x2 = tf.keras.Input(shape=(2,), name="bar")
+    @staticmethod
+    def save_test_model():
+        model = TestModelOpsChecker.create_dummy_model()
+        # save model in tmp as keras, tf2 saved_model and frozen graph
+        with tmp_dir(create=False, delete=False) as path_tf:
+            tf.saved_model.save(model, path_tf)
+        with tmp_dir(create=False, delete=False) as path_keras:
+            model.save(path_keras, overwrite=True, include_optimizer=False)
+        with tmp_dir(create=False, delete=False) as path_graph:
+            path_graph = Path(path_graph) / 'frozen_graph.pb'
+            cmsml_tools.save_graph(path=str(path_graph), obj=model, variables_to_constants=True,)
+
+        paths = {"keras": path_keras, "tf": path_tf, "frozen_graph": path_graph}
+        return paths
+
+    @staticmethod
+    def create_dummy_model():
+        x1 = tf.keras.Input(shape=(2,), name='foo')
+        x2 = tf.keras.Input(shape=(2,), name='bar')
         x = tf.concat([x1, x2], axis=1)
-        a1 = tf.keras.layers.Dense(10, activation="elu")(x)
-        y = tf.keras.layers.Dense(2, activation="softmax")(a1)
+        a1 = tf.keras.layers.Dense(10, activation='elu')(x)
+        y = tf.keras.layers.Dense(2, activation='softmax')(a1)
         model = tf.keras.Model(inputs=[x1, x2], outputs=y)
         return model
 
-    def test_set_flags(self):
-        def saved_model(mm):
-            self.assertTrue(mm.is_keras)
-            self.assertTrue(mm.is_saved_model)
-            self.assertFalse(mm.is_graph)
+    def test_load_graph_def(self):
+        # extract graph_def from keras, tf and frozen graphs
+        graph_graph_def = self.graph_model_ops.load_graph_def(
+            model_path=self.graph_model_ops.model_path, serving_key=self.serving_key)
+        tf_graph_def = self.tf_model_ops.load_graph_def(
+            model_path=self.tf_model_ops.model_path, serving_key=self.serving_key)
+        keras_graph_def = self.keras_model_ops.load_graph_def(
+            model_path=self.keras_model_ops.model_path, serving_key=self.serving_key)
+        potential_graph_defs = [graph_graph_def, tf_graph_def, keras_graph_def]
+        self.assertTrue(all([isinstance(graph_def, tf.compat.v1.GraphDef)
+                        for graph_def in potential_graph_defs]))
 
-        def graph_model(mm):
-            self.assertFalse(mm.is_keras)
-            self.assertFalse(mm.is_saved_model)
-            self.assertTrue(mm.is_graph)
+    def test_unique_ops(self):
+        # return unique ops of graph
+        should_have_ops = {'BiasAdd',
+                         'ConcatV2',
+                         'Const',
+                         'Elu',
+                         'Identity',
+                         'MatMul',
+                         'NoOp',
+                         'Softmax'}
 
+        # ReadVariableOp = Op to read out variable value
+        tf_keras_exclusive_ops = {'ReadVariableOp'}
+        # PlaceHolder = Placeholder for input tensors of graph
+        # in this case: placeholders have name 'foo', 'bar'
+        frozen_graph_exclusive_ops = {'Placeholder'}
+        tf_keras_ops = should_have_ops.union(tf_keras_exclusive_ops)
+        frozen_graph_ops = should_have_ops.union(frozen_graph_exclusive_ops)
 
-    def test_check_model_existance(self):
-        with self.assertRaises(ValueError):
-            mm = ModelLoader('non_existing')
-            mm.check_model_existance(mm.model_location)
+        self.assertEqual(self.tf_model_ops.unique_ops, tf_keras_ops)
+        self.assertEqual(self.keras_model_ops.unique_ops, tf_keras_ops)
+        self.assertEqual(self.graph_model_ops.unique_ops, frozen_graph_ops)
 
-        # should raise Exception for existing directories that are no tf.SavedModel
-        from tempfile import TemporaryDirectory
-        with TemporaryDirectory() as tmp_dir:
-            with self.assertRaises(Exception):
-                mm = ModelLoader(tmp_dir)
-                mm.check_model_existance(mm.model_location)
+    def test_find_match(self):
+        # should return list with tuples(Op, bool(exist in compatiblity table))
+        result_base = {('Elu', True),
+                       ('Softmax', True),
+                       ('MatMul', True),
+                       ('Const', True),
+                       ('BiasAdd', True),
+                       ('Identity', True),
+                       ('ConcatV2', True)}
 
-    def test_load_saved_model(self):
-        l_keras_model = self.mm_keras_model.load_saved_model(self.mm_keras_model.model_location)
-        l_tf_model = self.mm_tf_model.load_saved_model(self.mm_tf_model.model_location)
+        result_tf_keras_matching = result_base.union({('ReadVariableOp', True)})
 
-        self.assertIsInstance(l_keras_model, tf.keras.Model)
-        # instance of tensorflow api models is kinda hard to get
-        # instead check if isSavedModel and is not keras model
-        self.assertNotIsInstance(l_tf_model, tf.keras.Model)
-        self.assertTrue(tf.saved_model.contains_saved_model(self.mm_tf_model.model_location))
+        tf_matching = self.tf_model_ops.find_match(self.CPU_TABLE, "cpu")
+        keras_matching = self.keras_model_ops.find_match(self.CPU_TABLE, "cpu")
+        graph_matching = self.graph_model_ops.find_match(self.CPU_TABLE, "cpu")
 
-
-    def test_load_pb(self):
-        l_graph = self.mm_graph.load_pb(self.mm_graph.model_location)
-        self.assertIsInstance(l_graph, tf.compat.v1.Graph)
-
-    def test_load_model(self):
-        loaded_keras_model = self.mm_keras_model.load_model(self.mm_keras_model.model_location)
-        self.assertIsInstance(loaded_keras_model, tf.keras.Model)
-        loaded_tf_model = self.mm_tf_model.load_model(self.mm_tf_model.model_location)
-        self.assertIsInstance(loaded_tf_model, tf.keras.Model)
-
-class TestModelOpsChecker(unittest.TestCase):
-    def setUp(self):
-
-        test_dir = Path('test_models')
-        self.saved_keras_model = str(test_dir / 'test_keras_model')
-        self.saved_tf_model = str(test_dir / 'test_saved_model')
-        self.saved_graph = str(test_dir / 'test_freeze_graph.pb')
-
-        self.graph_model_ops = ModelOpsChecker(self.saved_graph)
-        self.keras_model_ops = ModelOpsChecker(self.saved_keras_model)
-        self.tf_model_ops = ModelOpsChecker(self.saved_tf_model)
-
-        self.default_signature = 'serving_default'
-
-
-    def test_get_graph_def(self):
-        # should return graph def of savedmodel and graph
-
-        graph_graph_def = self.graph_model_ops.get_graph_def(signature=self.default_signature)
-        tf_graph_def = self.tf_model_ops.get_graph_def(signature=self.default_signature)
-        self.assertEqual(graph_graph_def, tf_graph_def)
-
-    def test_get_node_def(self):
-        pass
+        self.assertEqual(tf_matching, result_tf_keras_matching)
+        self.assertEqual(keras_matching, result_tf_keras_matching)
+        # NoOp and Placeholder are filtered out
+        # XLA does not have compatiblity with these dummy ops.
+        self.assertEqual(graph_matching, result_base)
 
 
 if __name__ == '__main__':
