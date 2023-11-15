4ce04e1b7e2448fe8d83b8fa5801a66a33bad861
Marcel R <github.riga@icloud.com>
2023-11-15 18:16:42 +0100

    Update docker files.

--- cmsml/scripts/__init__.py
+++ cmsml/scripts/__init__.py
@@ -1,8 +1,6 @@
 # coding: utf-8
-__all__ = ["compile_tf_graph", "aot_compile",]
 
+__all__ = ["compile_tf_graph", "aot_compile"]
 
-# # provisioning imports
-from cmsml.scripts.compile_tf_graph import (
-    compile_tf_graph, aot_compile
-)
+# provisioning imports
+from cmsml.scripts.compile_tf_graph import compile_tf_graph, aot_compile
