e6eaf7aedf437da5fa30ee1c32bfce9dae8de0ed
Marcel Rieger <riga@users.noreply.github.com>
2024-02-16 09:00:06 +0100

    Fix provisioning imports in tf tools.

--- cmsml/tensorflow/__init__.py
+++ cmsml/tensorflow/__init__.py
@@ -5,15 +5,19 @@
 Classes, functions and tools for efficiently working with TensorFlow.
 """
 
-__all__ = ["import_tf", "save_frozen_graph", "load_frozen_graph", "write_graph_summary",
-"load_model", "load_graph_def","OpsData", "get_graph_ops"]
+__all__ = [
+    "import_tf", "save_frozen_graph", "save_graph", "load_frozen_graph", "load_graph",
+    "write_graph_summary", "load_model", "load_graph_def",
+    "OpsData", "get_graph_ops",
+]
 
 
 # provisioning imports
 from cmsml.tensorflow.tools import (
-    import_tf, save_frozen_graph, load_frozen_graph, write_graph_summary, load_model, load_graph_def
+    import_tf, save_frozen_graph, save_graph, load_frozen_graph, load_graph, write_graph_summary,
+    load_model, load_graph_def,
 )
 
 from cmsml.tensorflow.aot import (
-    OpsData, get_graph_ops
+    OpsData, get_graph_ops,
 )
